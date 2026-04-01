be16160aa77dcb1b4426d7afcda538ac8486fb88
Sergei <omgimgog@gmail.com>
2026-04-01 12:41:01 +0500

    Update home.py

--- WWIIhm/app/routers/home.py
+++ WWIIhm/app/routers/home.py
@@ -1,11 +1,14 @@
+import os
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import Place, User
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 router = APIRouter(tags=["home"])
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 def get_current_user(request: Request, db: Session = Depends(get_db)):
     user_id = request.session.get("user_id")
