1d33e7cd41b97b40d27faa1cc92fbefde868e203
Sergei <omgimgog@gmail.com>
2026-04-01 16:39:25 +0500

    Update home.py

--- WWIIhm/app/routers/home.py
+++ WWIIhm/app/routers/home.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import Place, User
-from ..templates_config import templates
+from ..templates_config import template_engine
 
 
 router = APIRouter(tags=["home"])
@@ -16,7 +16,7 @@ def get_current_user(request: Request, db: Session = Depends(get_db)):
         return db.query(User).filter(User.id == user_id).first()
     return None
 
-@router.get("/", response_class=templates.TemplateResponse)
+@router.get("/", response_class=template_engine.TemplateResponse)
 async def home(request: Request, db: Session = Depends(get_db)):
     places = db.query(Place).all()
     user = get_current_user(request, db)
@@ -30,14 +30,14 @@ async def home(request: Request, db: Session = Depends(get_db)):
         elif raw_image:
             main_map_image = str(raw_image)
     
-    return templates.TemplateResponse("index.html", {
+    return template_engine.TemplateResponse("index.html", {
         "request": request,
         "places": places,
         "user": user,
         "main_map_image": main_map_image
     })
 
-@router.get("/about", response_class=templates.TemplateResponse)
+@router.get("/about", response_class=template_engine.TemplateResponse)
 async def about(request: Request, db: Session = Depends(get_db)):
     user = get_current_user(request, db)
     return templates.TemplateResponse("about.html", {"request": request, "user": user})
