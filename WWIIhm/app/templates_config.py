051eaa7a1140ded19dbbe50df4eaa1f868cb577c
Sergei <omgimgog@gmail.com>
2026-04-01 16:37:38 +0500

    Update templates_config.py

--- WWIIhm/app/templates_config.py
+++ WWIIhm/app/templates_config.py
@@ -1,17 +1,9 @@
 import os
-
 from fastapi.templating import Jinja2Templates
-from jinja2 import FileSystemLoader, Environment
 
 # Абсолютный путь к папке templates
 TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 
-# Создаем окружение Jinja2 с отключенным кэшем
-jinja2_env = Environment(
-    loader=FileSystemLoader(TEMPLATES_DIR),
-    auto_reload=True,  # Перезагружать шаблоны при изменении
-    cache_size=0       # ОТКЛЮЧАЕМ КЭШ
-)
-
-# Создаем Jinja2Templates с нашим окружением
-templates = Jinja2Templates(env=jinja2_env)
+# Создаем объект шаблонов с отключенным кэшированием
+template_engine = Jinja2Templates(directory=TEMPLATES_DIR)
+template_engine.env.cache_size = 0  # Отключаем кэш
