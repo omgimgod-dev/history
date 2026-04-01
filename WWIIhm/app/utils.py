275f3f139ff8e62c2d076f6027400c3a4def32ae
Sergei <omgimgog@gmail.com>
2026-04-01 10:35:30 +0500

    Update utils.py

--- WWIIhm/app/utils.py
+++ WWIIhm/app/utils.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, Request
 from sqlalchemy.orm import Session
-from app.database import get_db
-from app.models import User
+from .database import get_db
+from .models import User
 
 def get_current_user(request: Request, db: Session = Depends(get_db)):
     user_id = request.session.get("user_id")
